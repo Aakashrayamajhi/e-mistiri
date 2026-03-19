dc95677458fbf2a19e9b77a1c673d20c3f10a3f1
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-03-19 15:05:02 +0545

    authservices setup complete ready to initialize

---
+++ server/services/auth-services/src/index.js
@@ -0,0 +1,18 @@
+import {app} from "./app.js"
+import { env } from "./config/dotenv.config.js"
+
+const PORT = env.PORT
+
+
+const StartServer =()=>{
+  try{
+    app.listen(PORT,()=>{
+      console.log(`server is running at port:${PORT}`)
+    })
+  } catch(error){
+    console.log('error:', error)
+
+  }
+}
+
+StartServer()
